ccc1ede624d7235c763a48011d37bf3f18960e46
Tina Liu <tina.chang.liu@gmail.com>
2019-05-27 14:31:04 +0900

    added first name, last_name, bio and photo to User model

---
+++ db/migrate/20190527052937_add_first_name_last_name_bio_photo_to_users.rb
@@ -0,0 +1,8 @@
+class AddFirstNameLastNameBioPhotoToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :bio, :text
+    add_column :users, :photo, :string
+  end
+end
