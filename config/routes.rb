b4edd1713ee1936e558c0ac00b4cbbc3eabe6d3b
Tina Liu <tina.chang.liu@gmail.com>
2019-05-28 10:59:01 +0900

    added routes and moved card into workshops view folder

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :workshops, except: [:destroy] do
+    resources :bookings, only: [:new, :create]
+  end
 end
