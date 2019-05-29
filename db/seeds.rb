f99317e4020a1e4ffa9f768a595f32106c44948e
Paulo Rivera <josepauloovallerivera@gmail.com>
2019-05-29 14:02:25 +0900

    - Added special seed for pitch presentation. - added the new category on the model. -fixed some bugs on the workshops  price and duration.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 # Some data to choose from.
 LEVEL = ["Beginner", "Intermediate", "Advanced"]
-CATEGORY = ["Calligraphy", "Ceramics", "Floristry", "Glass", "Jewellery", "Leather", "Paper", "Wood"]
+CATEGORY = ["Calligraphy", "Ceramics", "Floristry", "Glass", "Jewellery", 'Pottery', "Leather", "Paper", "Wood"]
 TITLES = ['Spoon carving for beginners', 'Green wood chair making in Suffolk', 'Don White Woodturning Experience']
 DURATION = [30, 60, 120, 180]
 BIOS = ['I have been been doing wood by miself and want to learn more.', 'Single, 9 cats, Love to craft stuff', 'Have a lot of time and want to work on crafting things', 'I love to create Jewelry and my family uses all my creations', 'I love this site is the best thing that happened to me!']
@@ -43,8 +43,8 @@ workshop_attributes = [
     name:         'Introduction to Modern Woodworking',
     description:  'Students will make a small table suitable for use as a planter or side table. It will be constructed out of oak using half-lap joints to create a flush assembly. The style is contemporary and minimalist, so it should fit well with any existing interior design.',
     category:      CATEGORY[-1],
-    address:       Faker::Address.full_address,
-    price:         rand(1..500),
+    address:       '1-chōme-3-24 Meguro, Meguro City, Tōkyō-to',
+    price:         8500,
     capacity:      rand(1..50),
     date_time:     Date.today + rand(1..30),
     owner_id:      User.all.sample.id,
@@ -55,8 +55,8 @@ workshop_attributes = [
     name:         'Japanese Woodworking Introduction',
     description:  'You will gain a broad understanding of how Japanese woodworking distinguishes itself from Western approaches during this two hour talk and demonstration. The session will include an overview of traditional hand tools used in a range of work scale, from making small objects to full size timber frame building.',
     category:      CATEGORY[-1],
-    address:       Faker::Address.full_address,
-    price:         rand(1..500),
+    address:       '1-chōme-25-17 Nakachō, Meguro City, Tōkyō-to',
+    price:         3000,
     capacity:      rand(1..50),
     date_time:      Date.today + rand(1..30),
     owner_id:       User.all.sample.id,
@@ -68,8 +68,8 @@ workshop_attributes = [
     description:  'Are you looking to put your creativity in to action? Then the jewelry class is definitely for you. Either if you are looking to make professional jewelry or just for a hobby then you have find the right jewelry class.
       Come and enjoy this 90-minute fabulous, in-depth Jewelry making class where you will learn how to make unique and gorgeous jewelry for everyday wear and those special nights out.',
     category:      CATEGORY[4],
-    address:       Faker::Address.full_address,
-    price:         rand(1..500),
+    address:       '2-chōme-9 Yaesu, Chuo City, Tōkyō-to',
+    price:         5000,
     capacity:      rand(1..50),
     date_time:     Date.today + rand(1..30),
     owner_id:       User.all.sample.id,
@@ -80,13 +80,25 @@ workshop_attributes = [
     name:         'Floral Design Sampler',
     description:  'Join us for a great day of flower design. The Sampler course is for individuals who want to try out our school and go home with a wealth of knowledge about the world of flower designing. It’s three great projects covering American to European design.',
     category:      CATEGORY[2],
-    address:       Faker::Address.full_address,
-    price:         rand(1..500),
+    address:       '1-chōme-33 Higashi, Shibuya City, Tōkyō-to',
+    price:         7300,
     capacity:      rand(1..50),
     date_time:     Date.today + rand(1..30),
     owner_id:       User.all.sample.id,
     level:          LEVEL.sample,
     duration:      DURATION.sample
+  },
+  {
+    name:         'Make A Mug Workshop',
+    description:  'The Make A Mug Workshop is a fun and easy introduction to working with clay. You will be shown how to construct a mug, with the choice of 4 different shapes. You will learn how to join pieces of clay together, including how to add a handle. You will be shown different decoration techniques so that you can decorate your mug. At the end of the workshop, you will get to choose which studio glaze you would like on your mug. It will then be fired and glazed for you, ready to collect at a later date. All materials are provided, as well as teas, coffees and biscuits. You won’t get that messy, but it is best to wear old clothes and shoes, just in case. This workshop is suitable for children over the age of 8. All children under the age of 16 must be accompanied by an adult.',
+    category:      CATEGORY[5],
+    address:       '1-chōme-7 Ariake, Koto City, Tōkyō-to',
+    price:         3000,
+    capacity:      rand(1..50),
+    date_time:     DateTime.new(2019,9,7,10),
+    owner_id:      User.all.sample.id,
+    level:         LEVEL.sample,
+    duration:      120
   }
 ]
   Workshop.create!(workshop_attributes)
@@ -98,6 +110,15 @@ workshop_attributes = [
   picture.workshop = workshops_arr[index_counter]
   index_counter += 1
   picture.save
+end
+
+puts 'Creating Pitch workshop photos.......'
+
+urls = ['https://static1.squarespace.com/static/57821680d1758e9af40b5460/t/5ac67cf36d2a73c1596bcec9/1522957579283/Turning+Earth+E10?format=1000w', 'https://static1.squarespace.com/static/57821680d1758e9af40b5460/5ac67d626d2a73c1596bebf5/5ac67e4e0e2e7211e5ca4f3c/1522958464330/_DSF2814.jpg?format=1500w', 'https://images.unsplash.com/photo-1556205801-a0bf81cdc90d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80', 'https://static1.squarespace.com/static/57821680d1758e9af40b5460/5ac67d626d2a73c1596bebf5/5ac684d1758d467fd1192596/1522959657588/20171225_TE_0384.jpg?format=1500w']
+urls.each do |url|
+  photo = Photo.new(workshop: workshops_arr.last)
+  photo.remote_image_url = url
+  photo.save
 end
 
 puts 'Workshop data added..'
