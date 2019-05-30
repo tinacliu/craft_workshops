761304604f4f380b85db581ae50fdc6cde0b2b15
Paulo Rivera <josepauloovallerivera@gmail.com>
2019-05-30 18:31:16 +0900

    New avatar feature. - Added a new field to upload a picture when creating a profile. - Fixed the profile page, now you can see your own profile picture. - Updated the models to support photos. - Updated the aplication controller"s strong params to accept the photos.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo])
   end
 end
