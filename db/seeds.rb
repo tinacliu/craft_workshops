43f0005f6ea0d161f125193dd50ed7f79c6668f8
Paulo Rivera <josepauloovallerivera@gmail.com>
2019-05-30 16:54:23 +0900

    quick fix on the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -127,7 +127,7 @@ puts 'creating bookings'
   Booking.create!(
     workshop: Workshop.all.sample,
     user: User.all.sample,
-    num_guests: rand(1..19)
+    num_guests: rand(1..6)
     )
 end
 
